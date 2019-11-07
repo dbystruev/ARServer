0a29d9ea3012afddee8a439dac499f7bbe3fe8ad
Denis Bystruev <github@learnswift.ru>
2019-11-07 22:36:37 +0300

    Implemented file download

--- Package.swift
+++ Package.swift
@@ -6,13 +6,15 @@ let package = Package(
     dependencies: [
       .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "2.8.0")),
       .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", from: "1.7.1"),
+      .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", from: "1.11.1"),
+      
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
     ],
     targets: [
       .target(name: "2019.10.28ARKitura", dependencies: [ .target(name: "Application") ]),
-      .target(name: "Application", dependencies: [ "Kitura", "HeliumLogger", "CloudEnvironment","SwiftMetrics", "Health", 
+      .target(name: "Application", dependencies: [ "Kitura", "HeliumLogger", "KituraStencil", "CloudEnvironment","SwiftMetrics", "Health",
 
       ]),
 
--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Kitura
+import KituraStencil
 import LoggerAPI
 import Configuration
 import CloudEnvironment
@@ -12,21 +13,58 @@ public let health = Health()
 public class App {
     let router = Router()
     let cloudEnv = CloudEnv()
-
+    
+    var rootDirectoryPath: String {
+        let fileManager = FileManager()
+        let currentPath = fileManager.currentDirectoryPath
+        return currentPath
+    }
+    
+    var rootDirectory: URL { URL(fileURLWithPath: rootDirectoryPath) }
+    var publicDirectory: URL { rootDirectory.appendingPathComponent("public") }
+    var uploadsDirectory: URL { publicDirectory.appendingPathComponent("uploads") }
+    var originalsDirectory: URL { uploadsDirectory.appendingPathComponent("originals") }
+    var thumbsDirectory: URL { uploadsDirectory.appendingPathComponent("thumbs") }
+    
     public init() throws {
         // Configure logging
         initializeLogging()
         // Run the metrics initializer
         initializeMetrics(router: router)
     }
-
+    
     func postInit() throws {
+        // Set Stencil as template engine
+        router.setDefault(templateEngine: StencilTemplateEngine())
+        
         // Endpoints
         initializeHealthRoutes(app: self)
-	router.all("/public", middleware: StaticFileServer())
-	router.all("/pikachu", middleware: StaticFileServer(path: "/Users/dbystruev/Documents/SwiftBook/Lessons/2019.10.28ARKitura/public/Pikachu"))
-   }
-
+        
+        print(#line, #function)
+        
+        router.all("/pikachu", middleware: StaticFileServer(path: "\(rootDirectoryPath)/public/Pikachu"))
+        
+        print(#line, #function)
+        
+        router.all("/originals", middleware: StaticFileServer(path: originalsDirectory.path))
+        
+        print(#line, #function)
+        
+        router.get("/public*") { request, response, next in
+            defer { next() }
+            
+            let fileManager = FileManager()
+            
+            guard let files = try? fileManager.contentsOfDirectory(
+                at: self.originalsDirectory,
+                includingPropertiesForKeys: nil,
+                options: .skipsHiddenFiles
+            ) else { return }
+            
+            try response.render("list", context: ["files": files.map { $0.lastPathComponent }])
+        }
+    }
+    
     public func run() throws {
         try postInit()
         Kitura.addHTTPServer(onPort: 9000 /*cloudEnv.port*/, with: router)
